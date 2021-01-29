6daafe6f9ccb7e2d6a6513a15e4b9ebecc3123d5
jamalmohamad <mohammed.jamalhadi@gmail.com>
2021-01-29 22:23:31 +0100

    feat: 85% of cv completion

--- src/app/cv-sections/education.component.ts
+++ src/app/cv-sections/education.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-education',
   template: `
-    <p>
-      education works!
-    </p>
+      <div class="aboutme-header"><p>EDUCATION</p></div>
+      <ul>
+          <li><p class="p-font">MSc in Computer Science, Specialization in Software and Service Architecture, Feb 2019 - July 2021 <i class="cv-i">Eötvös Loránd University (ELTE) (Budapest)</i></p></li>
+          <li><p class="p-font">MSc in Computer Science, Specialization in AI, Pécsi Tudományegytem <i class="cv-i">Dec 2018 - Jan 2019 (Pécs)</i></p></li>
+          <li><p class="p-font">BEng in Computer Technique Engineering, Dijla University college  <i class="cv-i">Baghdad-Iraq</i></p></li>
+      </ul>
   `,
   styles: [
   ]
